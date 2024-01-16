124b3ef74a31c62145887101698e9a775d560cdf
EbenezerOppong4127 <95159003+EbenezerOppong4127@users.noreply.github.com>
2024-01-16 21:56:37 +0000

    push7

---
+++ scripts/w02-task.js
@@ -0,0 +1,36 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+let fullName = 'Tro Opong Ebenezer Jules Samuel'
+let currentYear = 2024;
+
+const nameElement = document.getElementById(('name'));
+const foodElement = document.getElementById('food');
+
+const yearElement = document.querySelector('#year');
+const imageElement  =   document.querySelector('img[src="images/myself.jpg"]');
+/* Step 2 - Variables */
+
+
+
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = '<strong>${fullName}</strong>'
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
