a00688daf82a8beeb2d4669e0ce54c3f22719426
EbenezerOppong4127 <95159003+EbenezerOppong4127@users.noreply.github.com>
2024-02-08 00:39:02 +0000

    push13

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -6,28 +6,28 @@ let templeList = [];
 
 /* async displayTemples Function */
 const displayTemples = (temples) => {
-    // Clear the displayed list of temples
+    // Let   Clear the displayed list of temples
     reset();
 
     temples.forEach((temple) => {
-        // Create an HTML <article> element
+        // Let   Create an HTML <article> element
         const article = document.createElement('article');
 
-        // Create an HTML <h3> element and add the temple's templeName property
+        // Let   Create an HTML <h3> element and add the temple's templeName property
         const h3 = document.createElement('h3');
         h3.textContent = temple.templeName;
 
-        // Create an HTML <img> element and add the temple's imageUrl property to the src attribute
-        // and the temple's location property to the alt attribute.
+        // Let   Create an HTML <img> element and add the temple's imageUrl property to the src attribute
+        // Let   and the temple's location property to the alt attribute.
         const img = document.createElement('img');
         img.src = temple.imageUrl;
         img.alt = temple.location;
 
-        // Append the <h3> element and the <img> element to the <article> element as children.
+        // Let   Append the <h3> element and the <img> element to the <article> element as children.
         article.appendChild(h3);
         article.appendChild(img);
 
-        // Append the <article> element to the global templesElement variable declared above.
+        // Let   Append the <article> element to the global templesElement variable declared above.
         templesElement.appendChild(article);
     });
 };
@@ -35,51 +35,51 @@ const displayTemples = (temples) => {
 /* async getTemples Function using fetch()*/
 
 const getTemples = async () => {
-    const response = await fetch('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json');
+    const response = await fetch('https:// Let  byui-cse.github.io/cse121b-ww-course/resources/temples.json');
     templeList = await response.json();
     displayTemples(templeList);
 };
 
 /* reset Function */
 const reset = () => {
-    // Clear all <article> elements from the templesElement
+    // Let   Clear all <article> elements from the templesElement
     templesElement.innerHTML = '';
 };
 
 /* filterTemples Function */
 
-// Function: filterTemples()
+//   Function: filterTemples()
 const filterTemples = (temples) => {
-    // Clear the displayed list of temples
+    // Let Clear the displayed list of temples
     reset();
 
-    // Obtain the value of the HTML element with the ID of 'filtered' (The pull-down menu)
+    // Let Obtain the value of the HTML element with the ID of 'filtered' (The pull-down menu)
     const filter = document.getElementById('filtered').value;
 
-    // Use a switch statement that uses the filter value as the selector responding to four (4) cases.
+    // Let  Use a switch statement that uses the filter value as the selector responding to four (4) cases.
     switch (filter) {
         case 'utah':
-            // Filter for temples where the location contains "Utah" as a string.
+            // Let   Filter for temples where the location contains "Utah" as a string.
             displayTemples(temples.filter((temple) => temple.location.includes('Utah')));
             break;
         case 'nonutah':
-            // Filter for temples where the location does not contain "Utah" as a string.
+            // Let   Filter for temples where the location does not contain "Utah" as a string.
             displayTemples(temples.filter((temple) => !temple.location.includes('Utah')));
             break;
         case 'older':
-            // Filter for temples where the dedicated date is before 1950.
+            // Let   Filter for temples where the dedicated date is before 1950.
             displayTemples(temples.filter((temple) => new Date(temple.dedicatedDate) < new Date(1950, 0, 1)));
             break;
         case 'all':
         default:
-            // No filter. Just use temples as the argument.
+            // Let   No filter. Just use temples as the argument.
             displayTemples(temples);
             break;
     }
 };
 
-// Event Listener: filterTemples Element change
-document.getElementById('filtered').addEventListener('change', () => filterTemples(templeList));
+// Let   Event Listener: filterTemples Element change
+document.querySelector('#filtered').addEventListener('change', () => filterTemples(templeList));
 
 
 
